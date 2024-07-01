e61656ccff5488fc596454f273dca92f7e154012
p312388041 <312388041@qq.com>
2024-07-01 11:54:09 +0900

    add

--- src/main/java/com/chong/study/StudyApplication.java
+++ src/main/java/com/chong/study/StudyApplication.java
@@ -2,11 +2,19 @@ package com.chong.study;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-// @RestController
+@RestController
 public class StudyApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(StudyApplication.class, args);
+	}
+
+
+	@RequestMapping(value = "/hello")
+	public String hello() {
+		return "world";
 	}
 }
